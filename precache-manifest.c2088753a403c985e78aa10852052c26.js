2fa3aa704883d74ee4940c8ac7366cc19dc5f10e
Владислав <40837008+Wladislaw28@users.noreply.github.com>
2019-06-10 12:35:09 +0300

    Updates

---
+++ precache-manifest.c2088753a403c985e78aa10852052c26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373d702a2471189d270ae10b2a8f0b6a",
+    "url": "/react-test-todo/index.html"
+  },
+  {
+    "revision": "68ae8ce5a7c4a071eac0",
+    "url": "/react-test-todo/static/css/main.3e3dd75c.chunk.css"
+  },
+  {
+    "revision": "b301f76fd47ab1fff998",
+    "url": "/react-test-todo/static/js/2.2345bff9.chunk.js"
+  },
+  {
+    "revision": "68ae8ce5a7c4a071eac0",
+    "url": "/react-test-todo/static/js/main.4fdb76b8.chunk.js"
+  },
+  {
+    "revision": "8375c7eb3b5d29c93e72",
+    "url": "/react-test-todo/static/js/runtime~main.fcc119f8.js"
+  }
+]);
